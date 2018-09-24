d9ad7c18ee00b7205f50d48706648ec83ee5421a
Christian Geib <christian.geib79@gmail.com>
2018-09-24 23:50:56 +0100

    2nd commit homework completing calculator test not working because assertEquals not working

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,25 @@
 public class Calculator {
+    private int value1;
+    private int value2;
+
+    public Calculator(int value1, int value2){
+        this.value1 = value1;
+        this.value2 = value2;
+    }
+
+    public int add(int value1, int value2) {
+        return value1 + value2;
+    }
+
+    public int subtract(int value1, int value2) {
+        return value1 - value2;
+    }
+
+    public int multiply(int value1, int value2) {
+        return value1 * value2;
+    }
+
+    public int divide(int value1, int value2) {
+        return value1 / value2;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,37 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
- //test commit
+    Calculator calculator;
+
+@Before
+    public void before(){
+    calculator = new Calculator(4, 2);
+}
+
+@Test
+    public void add(){
+    int add = calculator.add(4,2);
+    assertEquals(6);
+}
+
+@Test
+    public void subtract(){
+    int subtract = calculator.subtract(4,2);
+    assertEquals(2);
+}
+
+@Test
+    public void multiply(){
+    int multiply = calculator.multiply(4,2);
+    assertEquals(8);
+}
+
+@Test
+    public void divide(){
+    int divide = calculator.divide(4, 2);
+    assertEquals(2);
+    }
 }
