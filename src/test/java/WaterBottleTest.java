9e9e27c947e3742574b01250e2b2e3215e4ec0a5
Christian Geib <christian.geib79@gmail.com>
2018-09-25 08:45:52 +0100

    4th commit homework completing calculator test still not working because assertEquals not working

--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,3 +1,36 @@
-public class WaterBottleTest {
+import org.junit.Before;
+import org.junit.Test;
 
-}
+
+    public class WaterBottleTest {
+
+
+        @Before
+        public void before() {
+            WaterBottle = new WaterBottle(100);
+        }
+
+
+        @Test
+        public void drink() {
+            int drink = WaterBottle.drink(100);
+            assertEquals(95);
+        }
+
+        @Test void currentVolume(){
+            int currentVolume = WaterBottle.getCurrentVolume(95);
+            assertEquals(95);
+        }
+
+        @Test void empty(){
+            int empty = WaterBottle.empty(100){
+                assertEquals(0);
+            }
+
+            @Test void fill(){
+                int fill = WaterBottle.fill(100){
+                    assertEquals(100);
+                }
+            }
+        }
+    }
